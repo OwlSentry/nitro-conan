c6b8f4ad8c9af1662f7120d8cf134457ae44356a
OwlSentry <rollbotsquared@gmail.com>
2026-04-30 14:23:32 +0000

    cleanup

--- recipes/nitro/test_package/conanfile.py
+++ recipes/nitro/test_package/conanfile.py
@@ -1,26 +1,19 @@
 import os
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.cmake import CMake, cmake_layout
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "VirtualBuildEnv", "VirtualRunEnv"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
     test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
 
-    def build_requirements(self):
-        self.tool_requires("cmake/[>=3.22 <4]")
-
     def layout(self):
         cmake_layout(self)
-
-    def generate(self):
-        CMakeToolchain(self).generate()
-        CMakeDeps(self).generate()
 
     def build(self):
         cmake = CMake(self)
@@ -29,5 +22,4 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, env="conanrun")
+            self.run(os.path.join(self.cpp.build.bindir, "test_package"), env="conanrun")
